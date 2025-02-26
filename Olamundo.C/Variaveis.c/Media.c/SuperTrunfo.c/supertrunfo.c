69c3d219d7180d46e033e9ce23fec77533aee317
vani202501120695 <202501120695@alunos.estacio.br>
2025-02-26 16:53:44 -0300

    Primeira depuração

---
+++ Olamundo.C/Variaveis.c/Media.c/SuperTrunfo.c/supertrunfo.c
@@ -0,0 +1,53 @@
+/*
+Gerar um programa que leia os dados de 2 Cartas
+Carta 1:
+
+Estado: A
+Código: A01
+Nome da Cidade: São Paulo
+População: 12325000
+Área: 1521.11 km²
+PIB: 699.28 bilhões de reais
+Número de Pontos Turísticos: 50
+
+Carta 2:
+
+Estado: B
+Código: B02
+Nome da Cidade: Rio de Janeiro
+População: 6748000
+Área: 1200.25 km²
+PIB: 300.50 bilhões de reais
+Número de Pontos Turísticos: 30   
+*/
+
+#include <stdio.h>
+
+int main (){
+    //Variável para char para Estado, Código, Nome
+    //Variavel para float área, PIB
+    //Variavel para int Carta, população, Número de Pontos Turísticos
+    char estado, cod;
+    char [50] nome;
+    float area, PIB;
+    int card, pop, NPT;
+
+        printf("Indique a posicao da carta: \n"); //Primeira ou 2ª carta
+        printf("Digite o Estado: \n"); //
+        printf("Digite o codigo da carta: \n");
+        printf("Qual o Nome da Cidade: \n");
+        printf("Qual o numero da populacao desse Estado: \n");
+        printf("Qual o tamanho da area do territorio da cidade: \n");
+        printf("Qual o PIB dessa cidade: \n");
+        printf("Quantos pontos turisticos existem: \n");
+
+        return 0;
+        
+
+
+
+}
+
+
+
+
